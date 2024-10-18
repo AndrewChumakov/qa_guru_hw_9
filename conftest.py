288754a7e38dab4101c9317c8b4ea69dbb3a2924
Andrew Chumakov <andrew.a.chumakov@yandex.ru>
2024-10-18 20:42:59 +0300

    add registration

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_driver():
+    driver_options = webdriver.FirefoxOptions()
+    driver_options.page_load_strategy = "eager"
+    browser.config.driver_options = driver_options
+    browser.config.base_url = "https://demoqa.com"
+    yield
+    browser.quit()
